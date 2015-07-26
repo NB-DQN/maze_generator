577d37088032d3df51ec6917efc2e696217bdabe
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-07-26 12:48:32 +0900

    wall and exit infomation

--- lib/maze.lua
+++ lib/maze.lua
@@ -14,3 +14,11 @@ function Maze.new(size_x, size_y)
   self.field = generate(size_x, size_y)
   return self
 end
+
+function Maze:wall(x, y)
+  return self.field[{ x, y, { 1, 4 } }]
+end
+
+function Maze:is_exit(x, y)
+  return self.field[x][y][5]
+end
