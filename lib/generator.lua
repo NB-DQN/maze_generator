a64c2dc9d367e17a99ee9fd1d5b7b39da6cde85c
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-07-26 12:06:39 +0900

    refactor

--- lib/generator.lua
+++ lib/generator.lua
@@ -9,35 +9,30 @@ function generate_field(size_x, size_y)
   field[start_cell] = 0
   local active_cells = { start_cell }
 
+  -- main backtracking loop
   while #active_cells > 0 do
     local current_cell = active_cells[#active_cells]
     local next_cell
 
     local candidate_cells = {
-      { current_cell[1] - 2, current_cell[2]     },
-      { current_cell[1],     current_cell[2] + 2 },
-      { current_cell[1] + 2, current_cell[2]     },
-      { current_cell[1],     current_cell[2] - 2 },
-    }
-    local path = {
-      { current_cell[1] - 1, current_cell[2]     },
-      { current_cell[1],     current_cell[2] + 1 },
-      { current_cell[1] + 1, current_cell[2]     },
-      { current_cell[1],     current_cell[2] - 1 },
+      { { current_cell[1] - 2, current_cell[2]     }, { current_cell[1] - 1, current_cell[2]     } },
+      { { current_cell[1],     current_cell[2] + 2 }, { current_cell[1],     current_cell[2] + 1 } },
+      { { current_cell[1] + 2, current_cell[2]     }, { current_cell[1] + 1, current_cell[2]     } },
+      { { current_cell[1],     current_cell[2] - 2 }, { current_cell[1],     current_cell[2] - 1 } },
     }
     local candidates = {}
     for direction = 1, 4 do
-      if field[candidate_cells[direction]] == 1 then
+      if field[candidate_cells[direction][1]] == 1 then
         candidates[#candidates + 1] = direction
       end
     end
 
     if #candidates > 0 then
       local direction = candidates[math.floor(torch.uniform(1, #candidates + 1))]
-      local next_cell = candidate_cells[direction]
+      local next_cell = candidate_cells[direction][1]
       field[next_cell] = 0
       active_cells[#active_cells + 1] = next_cell
-      field[path[direction]] = 0
+      field[candidate_cells[direction][2]] = 0
     else
       active_cells[#active_cells] = nil
     end
