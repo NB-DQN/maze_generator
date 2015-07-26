e913a22312695e13e74dab7877c6ed94c81adf05
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-07-26 11:15:20 +0900

    command line runner

--- lib/maze.lua
+++ lib/maze.lua
@@ -1,6 +1,6 @@
 require "generator"
 
-local Maze = {}
+Maze = {}
 Maze.__index = Maze
 
 setmetatable(Maze, {
@@ -14,6 +14,3 @@ function Maze.new(size_x, size_y)
   self.field = generate_field(size_x, size_y)
   return self
 end
-
--- run `th maze.lua`
--- print(Maze(10, 10).field)
